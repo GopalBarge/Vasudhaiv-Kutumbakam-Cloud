d0460aac50196c402871f5720da04b28e4e6b406
fti-gbarge <gbarge@ftilab.com>
2020-08-23 03:54:39 +0530

    added java8 lambda hellow world

--- api/cloudformation/deploy.sh
+++ api/cloudformation/deploy.sh
@@ -1,11 +1,10 @@
 pwd
 ls -lrt
 
-pushd ./api
-gradle clean build
-popd
+#pushd ./api
+#gradle clean build
+#popd
 
-pushd ./api/cloudformation
-echo 'pushd to ./api/cloudformation'
+pushd ./api
 pwd
 aws cloudformation deploy --template-file template.yaml --stack-name vk --capabilities CAPABILITY_NAMED_IAM
